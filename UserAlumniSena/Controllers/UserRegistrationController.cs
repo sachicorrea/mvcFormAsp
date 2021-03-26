977f4098122a39aa5fbe3526d5a35826b5b5212d
sachicorrea <scorrear@tutanota.com>
2021-03-25 18:42:53 -0700

    First commit

---
+++ UserAlumniSena/Controllers/UserRegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using UserAlumniSena.Models;
+
+namespace UserAlumniSena.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserRegClass ur)
+        {
+            string connection = "Data Source=DESKTOP-KU3FQ7M\\santi;Initial Catalog=MVCAlumniSena;Integrated Security=True";
+
+            using (SqlConnection sqlconn = new SqlConnection(connection))
+            {
+                string sqlquery = "insert into(Usr_doc, Usr_kind_doc, Usr_name, Usr_phonenum, Usr_email, Usr_gender, Usr_category, Usr_program, Usr_startdate, Usr_address, Usr_neighborhood, Usr_city, Usr_province, Usr_dateregistr) values('" +ur.Usr_doc+ "', '" +ur.Usr_kind_doc+ "', '" + ur.Usr_name + "', '" + ur.Usr_phonenum + "', '" + ur.Usr_email + "', '" + ur.Usr_gender + "', '" + ur.Usr_category + "', '" + ur.Usr_program + "', '" + ur.Usr_startdate + "', '" + ur.Usr_address + "', '" + ur.Usr_neighborhood + "', '" + ur.Usr_city + "', '" + ur.Usr_province + "', '" + ur.Usr_dateregistr + "')";
+
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    sqlcomm.ExecuteNonQuery();
+                    ViewData["Message"] = "The student/alumnus " + ur.Usr_name + " was saved correctly";
+                 }
+            }
+            
+            return View(ur);
+        }
+
+    }
+}
---
+++ UserAlumniSena/Models/UserRegClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserAlumniSena.Models
+{
+    public class UserRegClass
+    {
+        public int Usr_id { get; set; }
+
+        [Display(Name = "User id number")]
+        [Required(ErrorMessage = "Please enter your id number")]
+        public string Usr_doc { get; set; }
+
+        [Display(Name = "User document category")]
+        [Required(ErrorMessage = "Please enter your id category")]
+        public string Usr_kind_doc { get; set; }
+
+        [Display(Name = "User name")]
+        [Required(ErrorMessage = "Please enter user name")]
+        public string Usr_name { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Usr_phonenum { get; set; }
+
+        [Display(Name = "User name")]
+        [Required(ErrorMessage = "Please enter user name")]
+        public string Usr_email { get; set; }
+
+        [Display(Name = "Gender")]
+        [Required(ErrorMessage = "Please enter your gender")]
+        public char Usr_gender { get; set; }
+
+        [Display(Name = "Student's status")]
+        [Required(ErrorMessage = "Please enter your student's status")]
+        public string Usr_category { get; set; }
+
+        [Display(Name = "Program")]
+        [Required(ErrorMessage = "Please enter your program")]
+        public string Usr_program { get; set; }
+
+        [Display(Name = "Date Registration")]
+        [Required(ErrorMessage = "Please enter Date Registration")]
+        public DateTime Usr_startdate { get; set; }
+
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Please enter your address")]
+        public string Usr_address { get; set; }
+
+        [Display(Name = "Neighborhood")]
+        [Required(ErrorMessage = "Please enter your neighborhood")]
+        public string Usr_neighborhood { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Please enter your city")]
+        public string Usr_city { get; set; }
+
+        [Display(Name = "Province")]
+        [Required(ErrorMessage = "Please enter yoour province")]
+        public string Usr_province { get; set; }
+
+        [Display(Name = "Date registration at Sena")]
+        [Required(ErrorMessage = "Please enter Date registration at Sena")]
+        public DateTime Usr_dateregistr { get; set; }
+    }
+}
